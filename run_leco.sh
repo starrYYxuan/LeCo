34561702b214f4bd74fa80934df3641da92648b6
YYxuan99 <90175892+starrYYxuan@users.noreply.github.com>
2024-07-11 21:33:50 +0800

    Update run_leco.sh

--- run_leco.sh
+++ run_leco.sh
@@ -1,1 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 accelerate launch --num_processes=1 --main_process_port=12547 run_leco.py \
+-- test_set datasets/gsm8k.jsonl\
+-- prompts prompts/gsm8k/complex_step.txt\
+-- output_dir Your_output_dir\
+-- ckpt_dir Your_model_path\
